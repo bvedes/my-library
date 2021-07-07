7093b6e2838d03d35c05b126038f94d03440ef82
Benvinda Vedes <bvedes@BVAIR.local>
2021-07-07 19:47:35 +0100

    feat-button-back-home

--- Components/Layout/index.js
+++ Components/Layout/index.js
@@ -1,11 +1,3 @@
 import Layout from "./Layout";
 export default Layout;
-//https://img.freepik.com/free-photo/focused-female-customer-using-free-wi-fi-hotspot_74855-1894.jpg?size=626&ext=jpg
 
-/*const removeLibrary = () => {
-  handleSetLibraries({
-    ...libraries,
-    libraries: libraries.filter(([library]) => library !== library.id),
-  });
-};
-*/
